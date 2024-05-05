220b4f53eab0594aa7ad0a7dc5be475e20476cf0
bed-com-07-19 <bed-com-07-19@unima.ac.mw>
2024-05-06 00:32:48 +0200

    Updated code

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,17 +1,17 @@
-// import Topbar from "../../components/topbar/Topbar";
-// import Sidebar from "../../components/sidebar/Sidebar";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
 // import Feed from "../../components/feed/Feed";
-import Rightbar from "../../components/rightbar/Rightbar";
+// import Rightbar from "../../components/rightbar/Rightbar";
 import "./home.css";
 
 export default function Home() {
     return (
         <>
-            {/* <Topbar /> */}
+            <Topbar />
             <div className="homeContainer">
-                {/* <Sidebar />
-                <Feed />*/}
-                <Rightbar /> 
+                <Sidebar />
+                {/* <Feed />
+                <Rightbar />  */}
             </div>
         </>
     );
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -26,24 +26,3 @@ export default function Register() {
     </div>
   );
 }
-
-
-// import Topbar from "../../components/topbar/Topbar";
-// import Sidebar from "../../components/sidebar/Sidebar";
-// import Feed from "../../components/feed/Feed";
-// import Rightbar from "../../components/rightbar/Rightbar";
- {/* import "./home.css"
-
-export default function Home() {
-  return (
-    <>
-    <h1>Am Here</h1>
-     <Topbar />
-      <div className="homeContainer">
-        <Sidebar />
-        <Feed/>
-        <Rightbar/>
-      </div> 
-    </>
-  );
-}*/}
