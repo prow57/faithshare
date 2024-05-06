76964588672df70349f138ab6072f453a73e0539
bed-com-07-19 <bed-com-07-19@unima.ac.mw>
2024-05-06 02:05:50 +0200

    Made home as protected route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 // import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
+import Protected from './Protected';
 import {
   BrowserRouter as Router,
   Routes,
@@ -16,7 +17,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route exact path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        
+        <Route element={<Protected />}>
+            <Route exact path="/home" element={<Home />} />
+        </Route>
+        
         {/*<Route exact path="/" element={<Home />} />
          <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
